e2d92b6a15d0bd9ba21a762d69d74df625cf8a3a
DaGitFella <davibezerra123457@gmail.com>
2026-03-27 12:15:11 -0300

    feat: improving default button's micro interaction.

---
+++ app/components/Button/Default.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    @mouseover="showIcon = true"
+    @mouseleave="showIcon = false"
+    class="border flex items-center justify-center gap-2 py-2.5 px-7.5 rounded-xl lg:text-xl transition-all duration-500 ease-in-out cursor-pointer bg-indigo-50 text-indigo-700 border-indigo-200 hover:bg-indigo-700 hover:text-indigo-50 hover:border-indigo-500 hover:shadow-indigo-layered dark:bg-emerald-500 dark:border-emerald-600 dark:text-emerald-50 dark:hover:bg-emerald-50 dark:hover:text-emerald-700 dark:hover:border-emerald-500 dark:hover:shadow-emerald-layered"
+  >
+    <Transition>
+      <span
+        v-if="$slots.icon && showIcon"
+        v-motion
+        :initial="{ opacity: 0, x: 50 }"
+        :enter="{ opacity: 1, x: 0, scale: 1 }"
+        :leave="{
+          opacity: 0,
+          x: -50,
+          transition: {
+            type: 'spring',
+            stiffness: 250,
+            dumping: 25,
+            mass: 0.5,
+          },
+        }"
+        :delay="200"
+        :duration="300"
+        class="text-xl lg:text-2xl flex items-center"
+      >
+        <slot name="icon">icon</slot>
+      </span>
+    </Transition>
+    <slot name="text">text</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+const showIcon = ref(false);
+</script>
